029c626f6b1601932eeda2b85a6bca593b066e3e
chip2n <andreas@arvidsson.io>
2025-02-14 11:04:38 +0100

    Fetch dependencies manually using a script

---
+++ fetch-deps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Fetch the latest version of zball
+wget https://github.com/chip2n/zball/releases/latest/download/game.wasm -O assets/game.wasm
+wget https://github.com/chip2n/zball/releases/latest/download/game.js -O assets/game.js
